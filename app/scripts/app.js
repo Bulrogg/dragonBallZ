cd287671279106257841b0f3be2cf7132f1f4710
Frederic Millard <fmi@octo.com>
2014-07-14 00:11:07 +0200

    Init dargonBall app

---
+++ app/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dragonBallApp
+ * @description
+ * # dragonBallApp
+ *
+ * Main module of the application.
+ */
+angular.module('dragonBallApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dragonBallApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dragonBallApp
+ */
+angular.module('dragonBallApp').controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
