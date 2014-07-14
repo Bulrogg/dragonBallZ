d1fbfd5cd78b42e569e27fb926a3c8bafad91124
Frederic Millard <fmi@octo.com>
2014-07-14 19:32:55 +0200

    ajout de episode.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,1 @@
-'use strict';
-
-/**
- * @ngdoc overview
- * @name dragonBallApp
- * @description
- * # dragonBallApp
- *
- * Main module of the application.
- */
 angular.module('dragonBallApp', []);
