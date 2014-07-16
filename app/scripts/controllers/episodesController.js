5cc38e3a8bdf241a138db6646969a6d336c88c86
Frederic Millard <fmi@octo.com>
2014-07-16 12:01:06 +0200

    Clean code + ajout boutons episodeSuivant / episodePrecedent

--- app/scripts/controllers/episodesController.js
+++ app/scripts/controllers/episodesController.js
@@ -6,4 +6,20 @@ angular.module('dragonBallApp').controller('episodesController', ['$scope', 'epi
 
 	$scope.episodeCourant = $scope.episodes[0];
 
+	$scope.episodeSuivant = function() {
+		$scope.episodeCourant = $scope.episodes[$scope.episodes.indexOf($scope.episodeCourant) + 1];
+	};
+
+	$scope.episodePrecedent = function() {
+		$scope.episodeCourant = $scope.episodes[$scope.episodes.indexOf($scope.episodeCourant) - 1];
+	};
+
+	$scope.estLePremierEpisode = function() {
+		return $scope.episodeCourant && $scope.episodeCourant.id === '001'
+	};
+
+	$scope.estLeDernierEpisode = function() {
+		return $scope.episodeCourant && $scope.episodeCourant.id === '292';
+	}
+
 }]);
